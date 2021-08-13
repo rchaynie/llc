b394151c5829fb8bd7fa483c8f99679c97c36e4b
rchaynie <reidhaynie@gmail.com>
2021-08-13 19:08:58 -0400

    progress on about page.

--- flask/app.py
+++ flask/app.py
@@ -9,12 +9,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-        # writes raw data t0 a json file when a person accesses the page, so the viz code can use it
-    # with open("frontend/static/js/dogs.json", "w+") as f:
-    #     f.write(viz_data()) 
-    #     f.close() 
+      
 
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    
+
+    return render_template("about.html")
 
 
 
